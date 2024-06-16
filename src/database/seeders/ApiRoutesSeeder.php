864e73a43555821b2a1c1a007aa860db55094904
preto960 <preto.orellana@gmail.com>
2024-06-16 12:47:29 -0400

    update composer version require

--- src/database/seeders/ApiRoutesSeeder.php
+++ src/database/seeders/ApiRoutesSeeder.php
@@ -10,10 +10,10 @@ class ApiRoutesSeeder extends Seeder
     public function run()
     {
         $routes = [
-            ['type' => 'post', 'route' => 'register', 'controller' => 'Geekpack\Api\Http\Controllers\AuthController', 'class' => 'register', 'name' => 'api.register'],
-            ['type' => 'post', 'route' => 'login', 'controller' => 'Geekpack\Api\Http\Controllers\AuthController', 'class' => 'login', 'name' => 'api.login'],
-            ['type' => 'post', 'route' => 'logout', 'controller' => 'Geekpack\Api\Http\Controllers\AuthController', 'class' => 'logout', 'name' => 'api.logout'],
-            ['type' => 'post', 'route' => 'forgot-password', 'controller' => 'Geekpack\Api\Http\Controllers\AuthController', 'class' => 'forgotPassword', 'name' => 'api.forgot-password'],
+            ['type' => 'post', 'route' => 'api/register', 'controller' => 'Geekpack\Api\Http\Controllers\AuthController', 'class' => 'register', 'name' => 'api.register'],
+            ['type' => 'post', 'route' => 'api/login', 'controller' => 'Geekpack\Api\Http\Controllers\AuthController', 'class' => 'login', 'name' => 'api.login'],
+            ['type' => 'post', 'route' => 'api/logout', 'controller' => 'Geekpack\Api\Http\Controllers\AuthController', 'class' => 'logout', 'name' => 'api.logout'],
+            ['type' => 'post', 'route' => 'api/forgot-password', 'controller' => 'Geekpack\Api\Http\Controllers\AuthController', 'class' => 'forgotPassword', 'name' => 'api.forgot-password'],
         ];
 
         foreach ($routes as $route) {
